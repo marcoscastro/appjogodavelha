1d8820092fb563cc895d170de959a76ac19732a6
Marcos Castro <mcastrosouza@live.com>
2016-12-23 01:36:23 -0300

    added main activity

---
+++ app/src/main/java/com/mcastrosouza/jogodavelha/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mcastrosouza.jogodavelha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by marcos on 23/12/16.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.activity_main);
+    }
+}
