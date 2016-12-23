1f165df9133469162a3bc47db9795fc9fcaadfcf
Marcos Castro <mcastrosouza@live.com>
2016-12-23 02:39:35 -0300

    adicionada classe da activity do game

---
+++ app/src/main/java/com/mcastrosouza/jogodavelha/GameActivity.java
@@ -0,0 +1,17 @@
+package com.mcastrosouza.jogodavelha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by marcos on 23/12/16.
+ */
+
+public class GameActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/com/mcastrosouza/jogodavelha/MainActivity.java
+++ app/src/main/java/com/mcastrosouza/jogodavelha/MainActivity.java
@@ -1,17 +1,41 @@
 package com.mcastrosouza.jogodavelha;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by marcos on 23/12/16.
  */
 
 public class MainActivity extends Activity {
+    private String jogador1;
+    private String jogador2;
 
     @Override
-    public void onCreate(Bundle savedInstanceBundle) {
-        super.onCreate(savedInstanceBundle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void jogar(View v) {
+
+        // obtém os nomes dos jogadores
+        EditText editJogador1 = (EditText)findViewById(R.id.editJogador1);
+        EditText editJogador2 = (EditText)findViewById(R.id.editJogador2);
+
+        jogador1 = editJogador1.getText().toString();
+        jogador2 = editJogador2.getText().toString();
+
+        if(jogador1.isEmpty())
+            jogador1 = "Jogador1";
+        if(jogador2.isEmpty())
+            jogador2 = "Jogador2";
+
+        // chama a outra activity
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
